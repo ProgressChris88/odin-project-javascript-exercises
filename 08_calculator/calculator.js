a9c25432e4f53bae94c11a1a09d93d16863d6c0b
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-12-20 09:31:26 +0900

    Finished Calculator Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  let total = 0;
+  if (num.length == 0) {
+    total = 0;
+  } else {
+    for (let i = 0; i < num.length; i++) {
+      total += num[i];
+    }
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  let result = numArray[0];
+  for (let i = 1; i < numArray.length; i++) {
+    result = numArray[i] * result;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (factorialNum) {
+  if (factorialNum > 1) {
+    let result = factorialNum;
+    let count = factorialNum;
+    for (let i = 1; i < count; i++) {
+      result = result * (factorialNum - 1);
+      factorialNum = factorialNum - 1;
+    }
+    return result;
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +51,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
