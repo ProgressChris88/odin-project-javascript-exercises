96303f8b3f37b4a68a42392c28092942c8e0d172
Christopher Bowman <chrisbowmanfl@gmail.com>
2024-01-29 16:10:12 +0900

    Finished all but final test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // Compare the string forwards and backwards.
+  testStr = string.replace(/[^a-zA-ZS]/g, "").toLowerCase();
+  if (testStr == testStr.split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
