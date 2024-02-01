d8333976e48fed85758e860263d4e74196109e31
Christopher Bowman <chrisbowmanfl@gmail.com>
2024-02-01 18:30:22 +0900

    Finished Get The Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let resultArray = [];
+  for (let i = 0;i < books.length; i++)
+  {
+    resultArray.push(books[i].title)
+  }
+  return resultArray
 };
 
 // Do not edit below this line
